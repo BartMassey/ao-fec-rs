d0562ca232970eecb3a6b71f37430e793680d88e
Bart Massey <bart.massey@gmail.com>
2024-03-29 12:15:45 -0700

    updated to use bindgen in build.rs, compile with cc crate

--- build.rs
+++ build.rs
@@ -1,4 +1,16 @@
 fn main() {
+    let bindings = bindgen::Builder::default()
+        .allowlist_function("(ao_fec_encode|ao_fec_decode)")
+        .header("ao-fec/src/ao_fec.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("unable to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("ao_fec_ffi.rs"))
+        .expect("could not write bindings");
+
     cc::Build::new()
         .file("ao-fec/src/ao_fec_tx.c")
         .file("ao-fec/src/ao_fec_rx.c")
--- run-bindgen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-bindgen \
-    --allowlist-function 'ao_fec_encode|ao_fec_decode' \
-    ao-fec/src/ao_fec.h >src/ao_fec_ffi.rs
